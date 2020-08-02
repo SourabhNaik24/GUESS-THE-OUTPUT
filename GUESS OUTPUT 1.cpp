21ff1e068b195969de5ea42ea22ea7572566f78f
Sourabh Naik <43684440+SourabhNaik24@users.noreply.github.com>
2020-08-02 20:10:47 +0530

    Update GUESS OUTPUT 1.cpp

--- GUESS OUTPUT 1.cpp
+++ GUESS OUTPUT 1.cpp
@@ -9,9 +9,9 @@ int main()
 }
 
 /* Options: a) 0 9
-			b) 0 8
-			c) Compiler Error
-			d) No Output
+	    b) 0 8
+	    c) Compiler Error
+	    d) No Output
 
    Output: 0 8
    Explanation: As 0==1 is false the && condition becomes false so we get 0 and as this condition is not true the increment of 8 will not happen so we get 8
